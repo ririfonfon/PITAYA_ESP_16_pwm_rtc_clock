dbb4e08d3684c8ddaf36c94f0c5641f86f8b9c64
richard fontaine <ririfonfon10@gmail.com>
2023-02-08 17:29:15 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 
 #define DEBUG 1
+#define ID 12
 
 #include "variable.h"
 #include "led.h"
@@ -12,6 +13,8 @@
 #include "pwm_loop.h"
 #include "rtc_clock.h"
 #include "deep_sleep.h"
+#include "mqtt.h"
+#include "gps.h"
 
 void setup()
 {
@@ -21,50 +24,56 @@ void setup()
   Serial.println();
 #endif
 
-  // EEPROM
-  EEPROM.begin(EEPROM_SIZE);
-  init_eeprom();
+//   // EEPROM
+//   EEPROM.begin(EEPROM_SIZE);
+//   init_eeprom();
 
-  // init led
-  init_led();
+//   // init led
+//   init_led();
 
-  // init btn
-  init_btn();
+//   // init btn
+//   init_btn();
 
-  // init pwm
-  for (int k = 0; k < PWM_CHANNELS; k++)
-  {
-    ledcAttachPin(PWM_GPIOPIN[k], k);
-    ledcSetup(k, PWM_FREQUENCY, PWM_RESOLUTION);
-  }
+//   // init pwm
+//   for (int k = 0; k < PWM_CHANNELS; k++)
+//   {
+//     ledcAttachPin(PWM_GPIOPIN[k], k);
+//     ledcSetup(k, PWM_FREQUENCY, PWM_RESOLUTION);
+//   }
 
-  // random output
-  rnd();
-#ifdef DEBUG
-  for (int k = 0; k > FOR_PWM_CHANNELS; k++)
-  {
-    Serial.println(ref[k]);
-  }
-#endif
+//   // random output
+//   rnd();
+// #ifdef DEBUG
+//   for (int k = 0; k > FOR_PWM_CHANNELS; k++)
+//   {
+//     Serial.println(ref[k]);
+//   }
+// #endif
 
-  init_wifi();
-  WiFi.mode(WIFI_OFF);
+//   init_wifi();
+//   WiFi.mode(WIFI_OFF);
 
-  // init clock
-  init_clock();
+  // init_clock();
   // alarm_set();
   // deep_sleep_init();
+  init_mqtt();
+
+  delay(1000);
+  
+  init_gps();
 
 } // void setup()
 
 void loop()
 {
-  check_btn();
-  pwm_loop();
-  clock_loop();
-  if (deep)
-  {
-    deep = false;
-    deep_sleep_init();
-  }
+  // check_btn();
+  // pwm_loop();
+  // clock_loop();
+  // if (deep)
+  // {
+  //   deep = false;
+  //   deep_sleep_init();
+  // }
+
+  loop_gps();
 } // void loop()
