4a3c418e246c26a01a02f6a192a20bce86fe4a50
richard fontaine <ririfonfon10@gmail.com>
2023-02-09 14:30:39 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,10 @@
 #include "wifi_serv_setup.h"
 #include "btn.h"
 #include "pwm_loop.h"
-#include "rtc_clock.h"
-#include "deep_sleep.h"
 #include "mqtt.h"
 #include "gps.h"
+#include "rtc_clock.h"
+#include "deep_sleep.h"
 
 void setup()
 {
@@ -24,43 +24,45 @@ void setup()
   Serial.println();
 #endif
 
-//   // EEPROM
-//   EEPROM.begin(EEPROM_SIZE);
-//   init_eeprom();
+  //   // EEPROM
+  //   EEPROM.begin(EEPROM_SIZE);
+  //   init_eeprom();
 
-//   // init led
-//   init_led();
+  //   // init led
+  //   init_led();
 
-//   // init btn
-//   init_btn();
+  //   // init btn
+  //   init_btn();
 
-//   // init pwm
-//   for (int k = 0; k < PWM_CHANNELS; k++)
-//   {
-//     ledcAttachPin(PWM_GPIOPIN[k], k);
-//     ledcSetup(k, PWM_FREQUENCY, PWM_RESOLUTION);
-//   }
+  //   // init pwm
+  //   for (int k = 0; k < PWM_CHANNELS; k++)
+  //   {
+  //     ledcAttachPin(PWM_GPIOPIN[k], k);
+  //     ledcSetup(k, PWM_FREQUENCY, PWM_RESOLUTION);
+  //   }
 
-//   // random output
-//   rnd();
-// #ifdef DEBUG
-//   for (int k = 0; k > FOR_PWM_CHANNELS; k++)
-//   {
-//     Serial.println(ref[k]);
-//   }
-// #endif
+  //   // random output
+  //   rnd();
+  // #ifdef DEBUG
+  //   for (int k = 0; k > FOR_PWM_CHANNELS; k++)
+  //   {
+  //     Serial.println(ref[k]);
+  //   }
+  // #endif
 
-//   init_wifi();
-//   WiFi.mode(WIFI_OFF);
+  //   init_wifi();
+  //   WiFi.mode(WIFI_OFF);
 
-  // init_clock();
+  init_mqtt();
+  delay(1000);
+
+  init_gps();
+  delay(2000);
+
+  init_clock();
+
   // alarm_set();
   // deep_sleep_init();
-  init_mqtt();
-
-  delay(1000);
-  
-  init_gps();
 
 } // void setup()
 
